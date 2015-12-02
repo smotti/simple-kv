666d026795accfc6e1bc068f587f806a290fab6d
wott <william.ott@medicustek.com>
2015-12-02 20:47:57 +0800

    Add package def and server-socket

--- simple-kv.lisp
+++ simple-kv.lisp
@@ -2,7 +2,19 @@
 ;;; linear-lookup structures, that means traversing the whole list and
 ;;; comparing each key until one matches.
 
+(in-package :cl-user)
+
+(defpackage :simple-kv
+  (:use :cl :usocket))
+
+(in-package :simple-kv)
+
 (defvar *db* '())
+
+(setf *server-socket*
+  (usocket:socket-listen "127.0.0.1" 1234
+                         :reuse-address t
+                         :element-type 'unsinged-byte))
 
 (defun set-value (k v)
   (setf (getf *db* k) v))
