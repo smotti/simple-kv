fc62cb0194c61675bbf8ad179d92c68427cf91fd
wott <william.ott@medicustek.com>
2015-12-02 19:27:11 +0800

    Add description about implementation

--- simple-kv.lisp
+++ simple-kv.lisp
@@ -1,3 +1,7 @@
+;;; Naive implementation based on alist and/or plist which are both
+;;; linear-lookup structures, that means traversing the whole list and
+;;; comparing each key until one matches.
+
 (defvar *db* '())
 
 (defun set-value (k v)
