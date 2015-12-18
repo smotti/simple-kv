58533cae6456f9d9c642d92882019bfd8a88b654
wott <william.ott@medicustek.com>
2015-12-18 16:54:12 +0800

    Add implementation comment for persist function

--- simple-kv.lisp
+++ simple-kv.lisp
@@ -16,11 +16,25 @@
   "Delete the key from the store."
   (remhash key store))
 
-(defun persist (store path) '())
+;;; That following doesn't work, instead implement an event/fact based approach. This
+;;; means every set/get/delete is appended to a log sequence and on persist
+;;; written to disk. OPEN-KV-STORE then replays all of them. Note though that's
+;;; nothing for very large stores for that purpose we should write a bunch
+;;; of them to disk in intervals.
+;;; A hash-table is not a sequence!!
+(defun persist (path store)
+  "Persit the key-value store at the given path."
+  (with-open-file (out path
+                       :if-exists :supersede
+                       :if-does-not-exist :create
+                       :element-type '(unsigned-byte 8))
+    (write-sequence store out)))
 
 (defun open-kv-store (path) '())
 
 (defun new-kv-store ()
+  "Create a new key-value store and set is the *current-kv-store*. Returns a
+   new store (hash-table)."
   (let ((new-store (make-hash-table :test 'equal)))
     (setf *current-kv-store* new-store)))
 
